cdcd48405ca3d797ed564f8b0769400d858ae454
chicodevbr <chicodevbr@gmail.com>
2022-04-26 09:34:57 -0300

    without observable

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import TodoInput from './Todo/TodoInput';
 import TodoList from './Todo/TodoList';
+import styles from './App.module.css';
+import { useState } from 'react';
 
 const App = () => {
+  const [todosVisible, setTodosVisible] = useState(true);
+
+  const handleClick = () => setTodosVisible((state) => !state);
+
   return (
     <div className="app">
       <TodoInput />
-      <TodoList />
+      <div className={styles['todo-list-wrapper']}>
+        <h2 onClick={handleClick}>
+          <span>{todosVisible ? '-' : '+'}</span>
+          Todos
+        </h2>
+      </div>
+      {todosVisible && <TodoList />}
     </div>
   );
 };
