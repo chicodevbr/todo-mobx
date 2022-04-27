2e7f9bd41183836e71b9d531b311af7a3e4beaeb
chicodevbr <chicodevbr@gmail.com>
2022-04-27 10:45:59 -0300

    with async/await

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,23 @@ import TodoInput from './Todo/TodoInput';
 import TodoList from './Todo/TodoList';
 import styles from './App.module.css';
 import { observer, useLocalObservable } from 'mobx-react-lite';
-import { observable, runInAction } from 'mobx';
+import { runInAction } from 'mobx';
 
 const App = () => {
   const appUI = useLocalObservable(() => ({
     todosVisible: true,
     loading: false,
-    toggleTodoVisibility() {
+    async toggleTodoVisibility() {
       this.loading = true;
 
-      new Promise((resolve) => setTimeout(() => resolve(void 0), 1000)).then(
-        () => {
-          runInAction(() => {
-            this.loading = false;
-            this.todosVisible = !appUI.todosVisible;
-          });
-        }
-      );
+      await new Promise((resolve) => setTimeout(() => resolve(void 0), 500));
+
+      runInAction(() => {
+        this.loading = false;
+        this.todosVisible = !appUI.todosVisible;
+      });
     },
   }));
-
-  const todosVisible = observable.box(true);
-  todosVisible.observe_(({ newValue }) => {
-    console.log('the new value is', newValue);
-  });
-  todosVisible.set(false);
-  todosVisible.set(true);
 
   return (
     <div className="app">
