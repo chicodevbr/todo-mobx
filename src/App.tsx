b44e532c22946debd6feca7fb56bb5d27ad73902
chicodevbr <chicodevbr@gmail.com>
2022-04-28 09:22:28 -0300

    add autorun

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import TodoList from './Todo/TodoList';
 import styles from './App.module.css';
 import { observer, useLocalObservable } from 'mobx-react-lite';
 import { useStore } from './stores';
+import { useEffect } from 'react';
+import { autorun } from 'mobx';
 
 const App = () => {
   const { todos } = useStore();
+
+  useEffect(() => {
+    autorun(() => {
+      console.log(todos.list.length);
+    });
+  }, [todos.list]);
 
   const appUI = useLocalObservable(() => ({
     todosVisible: true,
